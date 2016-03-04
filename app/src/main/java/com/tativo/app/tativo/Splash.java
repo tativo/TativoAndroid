a603a04681ca7bcc33fb6735e05f8d81abb3af8f
daniel <skyline_medina@hotmail.com>
2016-03-04 11:21:41 -0700

    comit

--- app/src/main/java/com/tativo/app/tativo/Splash.java
+++ app/src/main/java/com/tativo/app/tativo/Splash.java
@@ -1,7 +1,43 @@
 package com.tativo.app.tativo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.tativo.app.tativo.LogIn.Actividades.Act_Cotizador;
+import com.tativo.app.tativo.LogIn.Actividades.Act_LogIn;
+
 /**
  * Created by SISTEMAS1 on 01/03/2016.
  */
-public class Splash {
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(Splash.this,Act_Cotizador.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
 }
