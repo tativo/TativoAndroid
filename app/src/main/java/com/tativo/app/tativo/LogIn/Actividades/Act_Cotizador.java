6b3cbfca9d5184f26a5fddbb3974d478968b38cf
amendez0706 <alfonso.mendez@balor.mx>
2016-03-08 09:03:31 -0700

    Cambios del cotizador

--- app/src/main/java/com/tativo/app/tativo/LogIn/Actividades/Act_Cotizador.java
+++ app/src/main/java/com/tativo/app/tativo/LogIn/Actividades/Act_Cotizador.java
@@ -1,15 +1,20 @@
 package com.tativo.app.tativo.LogIn.Actividades;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.tativo.app.tativo.R;
 
 public class Act_Cotizador extends AppCompatActivity {
 
+       
+    Button btnListo;
+    TextView lblTitulo1,lblTitulo2,lblTitulo3;
 
 
     @Override
@@ -17,6 +22,26 @@ public class Act_Cotizador extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_cotizador);
 
+        lblTitulo1 = (TextView) findViewById(R.id.lblTitulo1);
+        lblTitulo1.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/SFDigitalReadout-Light.ttf"));
+        lblTitulo1.setText("¡CUMPLE");
 
+        lblTitulo2 = (TextView) findViewById(R.id.lblTitulo2);
+        lblTitulo2.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/SFDigitalReadout-Light.ttf"));
+        lblTitulo2.setText("TUS DESEOS");
+
+        lblTitulo3 = (TextView) findViewById(R.id.lblTitulo3);
+        lblTitulo3.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/SFDigitalReadout-Light.ttf"));
+        lblTitulo3.setText("AQUI!");
+
+        btnListo = (Button) findViewById(R.id.btnListo);
+        btnListo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Act_LogIn.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
