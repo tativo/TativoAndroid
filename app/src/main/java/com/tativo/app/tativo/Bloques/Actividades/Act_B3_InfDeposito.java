82d07d77b20202d55291df470a9af27b8c438f8b
tativo <tativo.cln@gmail.com>
2016-03-11 16:58:37 -0700

    correccion de aspectos visuales

--- app/src/main/java/com/tativo/app/tativo/Bloques/Actividades/Act_B3_InfDeposito.java
+++ app/src/main/java/com/tativo/app/tativo/Bloques/Actividades/Act_B3_InfDeposito.java
@@ -5,12 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.tativo.app.tativo.R;
 
 public class Act_B3_InfDeposito extends AppCompatActivity {
 
-    Button btnDatosDeposito;
+    Button btnDatosDeposito, btnCardIO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class Act_B3_InfDeposito extends AppCompatActivity {
         setContentView(R.layout.act_b3_infdeposito);
 
         btnDatosDeposito = (Button) findViewById(R.id.btnDatosDeposito);
+        btnCardIO = (Button) findViewById(R.id.btnCardIO);
 
         btnDatosDeposito.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +29,13 @@ public class Act_B3_InfDeposito extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnCardIO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Escanear tarjeta",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
