95f3bd12cd30f0c5efe115b67b048cd89f360ceb
tativo <tativo.cln@gmail.com>
2016-03-11 10:30:59 -0700

    carga de fragmento en actividad LogIn

--- app/src/main/java/com/tativo/app/tativo/LogIn/Actividades/Act_LogIn.java
+++ app/src/main/java/com/tativo/app/tativo/LogIn/Actividades/Act_LogIn.java
@@ -1,32 +1,43 @@
 package com.tativo.app.tativo.LogIn.Actividades;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.Button;
 
-import com.tativo.app.tativo.Bloques.Actividades.Act_B1_Referencias;
+import com.tativo.app.tativo.LogIn.Fragmentos.Frg_LogIn;
+import com.tativo.app.tativo.LogIn.Fragmentos.Frg_Registro;
 import com.tativo.app.tativo.R;
 
-public class Act_LogIn extends AppCompatActivity {
+public class Act_LogIn extends FragmentActivity {
 
-    Button btnRegistro;
-
+    Button btnfrgLogIn, btnfrgRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_login);
 
-        btnRegistro = (Button) findViewById(R.id.btnRegistro);
-
-        btnRegistro.setOnClickListener(new View.OnClickListener() {
+        btnfrgLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), Act_B1_Referencias.class);
-                startActivity(i);
-                finish();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Frg_LogIn newFragment = new Frg_LogIn();
+                transaction.replace(R.id.flContenedor, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        btnfrgRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Frg_Registro newFragment = new Frg_Registro();
+                transaction.replace(R.id.flContenedor, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
     }
--- app/src/main/java/com/tativo/app/tativo/LogIn/Fragmentos/Frg_Registro.java
+++ app/src/main/java/com/tativo/app/tativo/LogIn/Fragmentos/Frg_Registro.java
@@ -1,8 +1,8 @@
 package com.tativo.app.tativo.LogIn.Fragmentos;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +13,12 @@ import com.tativo.app.tativo.R;
  * Created by SISTEMAS1 on 29/02/2016.
  */
 public class Frg_Registro extends Fragment {
+
+    public Frg_Registro()
+    {
+
+    }
+
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
