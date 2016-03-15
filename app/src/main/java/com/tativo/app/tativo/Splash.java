698241beeb4f5bb8cd8e310b7b3940a7b796a426
tativo <tativo.cln@gmail.com>
2016-03-15 12:15:37 -0700

    cambio de gif pantalla Splash

--- app/src/main/java/com/tativo/app/tativo/Splash.java
+++ app/src/main/java/com/tativo/app/tativo/Splash.java
@@ -22,7 +22,7 @@ public class Splash extends Activity {
         Thread timerThread = new Thread(){
             public void run(){
                 try{
-                    sleep(3000);
+                    sleep(4000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
