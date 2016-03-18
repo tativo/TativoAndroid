6306d6aa4cb8a18139b9a9fbe207c1857c02debe
tativo <tativo.cln@gmail.com>
2016-03-18 08:32:11 -0700

    Catcolonia

---
+++ app/src/main/java/com/tativo/app/tativo/Bloques/Clases/Catcolonia.java
@@ -0,0 +1,34 @@
+package com.tativo.app.tativo.Bloques.Clases;
+
+/**
+ * Created by SISTEMAS1 on 18/03/2016.
+ */
+public class Catcolonia {
+    private String Ciudadid;
+    private String Coloniaid;
+    private String Colonia;
+
+    public String getCiudadid() {
+        return Ciudadid;
+    }
+
+    public void setCiudadid(String ciudadid) {
+        Ciudadid = ciudadid;
+    }
+
+    public String getColoniaid() {
+        return Coloniaid;
+    }
+
+    public void setColoniaid(String coloniaid) {
+        Coloniaid = coloniaid;
+    }
+
+    public String getColonia() {
+        return Colonia;
+    }
+
+    public void setColonia(String colonia) {
+        Colonia = colonia;
+    }
+}
