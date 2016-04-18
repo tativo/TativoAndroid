d1e4d49ceda3a5e502c162e911c9acd8cb4319df
tativo <tativo.cln@gmail.com>
2016-04-18 09:33:46 -0600

    datos contrato

--- app/src/main/java/com/tativo/app/tativo/Operaciones/Fragmentos/Frg_Contrato.java
+++ app/src/main/java/com/tativo/app/tativo/Operaciones/Fragmentos/Frg_Contrato.java
@@ -2,23 +2,19 @@ package com.tativo.app.tativo.Operaciones.Fragmentos;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-
 import com.tativo.app.tativo.R;
 
 /**
  * Created by SISTEMAS1 on 09/04/2016.
  */
-public class Frg_Contrato extends DialogFragment {
+public class Frg_Contrato extends DialogFragment  {
 
     private static final String TAG = Frg_Contrato .class.getSimpleName();
 
@@ -41,22 +37,18 @@ public class Frg_Contrato extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-
-        View v = inflater.inflate(R.layout.frg_contrato_caratula, null);
-
+        View v = inflater.inflate(R.layout.frg_contrato, null);
         builder.setView(v);
 
-        Button ir = (Button) v.findViewById(R.id.btnIrARegistro);
-
-        ir.setOnClickListener(new View.OnClickListener() {
+        Button sig = (Button) v.findViewById(R.id.btnCaratulaContrato);
+        sig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getActivity(), Frg_Contrato.class);
-                startActivity(i);
-                getActivity().finish();
+
+   
+
             }
         });
-
 
         return builder.create();
     }
